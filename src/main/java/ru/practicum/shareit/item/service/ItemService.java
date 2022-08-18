5b6e40dc6615ffac231b57c99683946257277d44
Mikhail <ezar4992@yandex.ru>
2022-08-18 20:48:02 +0400

    <fix2><первый вариант тз13>

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -5,9 +5,13 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
-     ItemDto create(ItemDto user, long userId);
-     ItemDto update(ItemDto user, long itemId, long userId);
-     ItemDto findItemById(long id);
-     List<ItemDto> findOwnersItems(long userId);
-     List<ItemDto> findAvailableItems(String text);
+    ItemDto create(ItemDto user, long userId);
+
+    ItemDto update(ItemDto user, long itemId, long userId);
+
+    ItemDto findItemById(long id);
+
+    List<ItemDto> findOwnersItems(long userId);
+
+    List<ItemDto> findAvailableItems(String text);
 }
